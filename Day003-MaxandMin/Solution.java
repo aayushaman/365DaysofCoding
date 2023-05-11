6059a91c1f4c820486a6e73f9ddac666bcee0ff3
Elliot Alderson <popos@pop-os.localdomain>
2023-05-12 01:51:05 +0530

    Add Max and Min Problem

---
+++ Day003-MaxandMin/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = array[0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("The maximum element is " + max);
+        System.out.println("The minimum element is " + min);
+    }
+}
