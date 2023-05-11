0b084f85047b4ad59983f5981f6ab71bdc4b96ee
Elliot Alderson <popos@pop-os.localdomain>
2023-05-12 01:42:31 +0530

    Add Reverse the Array

---
+++ Day002-ReverseTheArray/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public static void reverseArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        reverseArray(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
